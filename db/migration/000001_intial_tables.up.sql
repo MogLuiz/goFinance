6e02e6ee298e21b742668cd5695f98997cb88817
Luiz Henrique <luizhjramos@outlook.com>
2022-10-01 22:16:34 -0300

    feat: categories database table

--- db/migration/000001_intial_tables.up.sql
+++ db/migration/000001_intial_tables.up.sql
@@ -5,3 +5,14 @@ CREATE TABLE "users" (
   "email" varchar UNIQUE NOT NULL,
   "created_at" timestamptz NOT NULL DEFAULT (now())
 );
+
+CREATE TABLE "categories" (
+  "id" serial PRIMARY KEY NOT NULL,
+  "user_id" int NOT NULL,
+  "title" varchar NOT NULL,
+  "type" varchar NOT NULL,
+  "description" varchar NOT NULL,
+  "created_at" timestamptz NOT NULL DEFAULT (now())
+);
+
+ALTER TABLE "categories" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
